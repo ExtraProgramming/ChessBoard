d3097f9f85e95f48165e81f56f2fa89ad5846584
Jonas Myhr Refseth <jonas@oisann.net>
2015-09-29 16:41:51 +0200

    Updated to a more flexible board size

--- Assets/Scripts/ChessBoardGenerator.cs
+++ Assets/Scripts/ChessBoardGenerator.cs
@@ -4,21 +4,24 @@ using System.Collections;
 public class ChessBoardGenerator : MonoBehaviour {
     public GameObject whiteTile, blackTile;
     public float size = 1.5f;
+	public int hW = 8;
 
     //private GameObject[,] chessBoard;
     private string[] letters = new string[8] {"A", "B", "C", "D", "E", "F", "G", "H" };
     private GameObject currentTile;
+	private float startPos = 0f;
 
     void Start () {
+		startPos = 0 - (size * (hW/2));
         //chessBoard = new GameObject[8, 8];
         generateChessboard();
     }
 
     void generateChessboard() {
         currentTile = blackTile;
-        for(int n = 1; n <= 8; n++) {
-            for(int l = 0; l < 8; l++) {
-                GameObject temp = (GameObject) Instantiate(currentTile, new Vector3(-6f + (n * size), -6f + (l * size), 0), Quaternion.identity);
+		for(int n = 1; n <= hW; n++) {
+            for(int l = 0; l < hW; l++) {
+				GameObject temp = (GameObject) Instantiate(currentTile, new Vector3(startPos + (n * size), startPos + (l * size), 0), Quaternion.identity);
                 temp.transform.parent = transform;
                 temp.transform.name = letters[l] + n;
                 //chessBoard[l, n-1] = temp;
@@ -34,5 +37,4 @@ public class ChessBoardGenerator : MonoBehaviour {
         }
         currentTile = whiteTile;
     }
-
 }
